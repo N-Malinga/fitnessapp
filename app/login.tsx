f1f3d7d2d6500879acfb90d733ffbe017cb2c54c
Nipun Malinga Premalal <nipunmalinga5@gmail.com>
2024-12-27 16:17:09 +0530

    iitial

---
+++ app/login.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+
+// Login Component
+const Login = () => {
+    return (
+        <View>
+            <Text>Login Page</Text>
+        </View>
+    );
+};
+
+export default Login;
